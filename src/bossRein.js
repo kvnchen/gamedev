c6670dcf75513c7eae4daeff202798308a949493
Kelvin Chen <kelvin.c.chen@gmail.com>
2016-02-28 17:42:26 -0800

    to do: reingod attacks and states

--- src/bossRein.js
+++ src/bossRein.js
@@ -1,20 +1,29 @@
 /*
  *  Reindeer god boss
- *
- *  I still don't have a good name for it shit
  */
 
 var enemy = require('./enemy');
 
 function Reingod() {
   enemy.call(this, 'Reingod');
-  this.maxHealth = 64;
-  this.currentHealth = 64;
-  this.goldDrop = 1000;
+  this.maxHealth = 128;
+  this.currentHealth = 128;
+  this.goldDrop = 10000;
+  this.targetAreas = ['head', 'wound'];
+  this.attacks = ['Charge', 'Frost Breath'];
+  this.validSpellTargets = {
+    head: ['Ignite'],
+    wound: ['Ignite','Push','Pull','Heal']
+  };
+  this.woundState = 'frozen'; // embedded, embedded hot, embedded deeply, raw, healed
 }
 
 Reingod.prototype = Object.create(enemy.prototype);
 
 Reingod.prototype.constructor = Reingod;
+
+Reingod.prototype.charge = function(player) {
+
+};
 
 module.exports = Reingod;
--- src/enemy.js
+++ src/enemy.js
@@ -5,20 +5,39 @@
  *  simply being defined in a data file. 
  */
 
+var _ = require('underscore');
+
 function Enemy(name) {
   this.name = name;
   this.maxHealth = 1;
   this.currentHealth = 1;
+  this.targetAreas = [];
+  this.attacks = [];
   this.buffs = [];
   this.debuffs = [];
   this.goldDrop = 0;
   this.itemDrops = [];
 }
 
+Enemy.prototype.getArrProps = function(prop) {
+  if (this[prop].length === 0) {
+    return 'none';
+  } else {
+    var str = '';
+    _.each(this[prop], function(p) {
+      str += p +', ';
+    });
+    return str;
+  }
+};
+
 Enemy.prototype.getStatus = function() {
   var status = 'Status of ' + this.name + ':\n';
 
   status += 'Current health: ' + this.currentHealth + '/' + this.maxHealth + '\n';
+  status += 'Targetable areas: ' + this.getArrProps('targetAreas') + '\n';
+  status += 'Attacks: ' + this.getArrProps('attacks') + '\n';
+  status += 'Gold reward: ' + this.goldDrop + '\n';
 
   return status;
 };
