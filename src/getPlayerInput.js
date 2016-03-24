4ef68b818594855bcce332f2d5a3a7ad159e8570
Kelvin Chen <kcchen@us.ibm.com>
2016-03-24 09:45:15 -0700

    game ai fun

--- src/getPlayerInput.js
+++ src/getPlayerInput.js
@@ -2,8 +2,20 @@
  *  Get player input from stdin
  */
 
+var _ = require('underscore');
+
 // temp no-internet workaround. burn them all muahahahaahaaaa
 function getPlayerInput(player, enemy) {
+  if (player.currentHealth <= 10) {
+    return player.castSpell('Heal',player);
+  }
+  if (_.contains(player.getDebuffs(), 'frozen')) {
+    if (player.currentHealth - 10 <= 5) {
+      return player.castSpell('Heal',player);
+    } else {
+      return player.castSpell('Ignite',player);
+    }
+  }
   return player.castSpell('Ignite',enemy,'wound');
 }
 
